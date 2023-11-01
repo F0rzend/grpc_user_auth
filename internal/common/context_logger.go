04ccf0d0ed82b9d0a324ce735ca71e0da8efaf17
F0rzend <zhogalkostya@gmail.com>
2023-11-01 06:22:15 +0300

    Implement base logic

---
+++ internal/common/context_logger.go
@@ -0,0 +1,21 @@
+package common
+
+import (
+	"context"
+	"log/slog"
+)
+
+type slogContextKey struct{}
+
+func InjectLogger(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, slogContextKey{}, logger)
+}
+
+func ExtractLogger(ctx context.Context) *slog.Logger {
+	logger, ok := ctx.Value(slogContextKey{}).(*slog.Logger)
+	if !ok {
+		return slog.New(new(disabledLoggerHandler))
+	}
+
+	return logger
+}
